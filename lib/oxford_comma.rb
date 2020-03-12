879c17dc9b9de7d66d007f0397de564074081da4
Daniel Amyakar <dan.rockit@gmail.com>
2020-03-12 00:37:43 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,17 +4,19 @@ a3 = ["kiwi", "durian", "starfruit"]
 a4 = ["kiwi", "durian", "starfruit", "mangos", "dragon fruits", "lychees", "pomelos"]
 
 def oxford_comma(a)
-
- if a.length > 1
-  myStr = "" 
-  i = 0
+  if a.length == 1
+    return a.join
+  elsif a.length == 2
+    return a.join(" and ")  
+  else 
+    myStr = "" 
+    i = 0
     while i <= a.length - 2
       myStr += (a[i]+", ")
       i += 1
     end
-     myStr += ("and " + a.last)
- else
-     return a.join
+    
+    return myStr += ("and " + a.last)
  end
 end
 
