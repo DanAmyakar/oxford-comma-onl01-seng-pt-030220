6e363b00e4bf892bcc521bae10ab171f3b3a31f9
Daniel Amyakar <dan.rockit@gmail.com>
2020-03-12 00:21:13 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,24 @@
-def oxford_comma(array)
+a1 = ["kiwi"]
+a2 = ["kiwi", "durian"]
+a3 = ["kiwi", "durian", "starfruit"]
+a4 = ["kiwi", "durian", "starfruit", "mangos", "dragon fruits", "lychees", "pomelos"]
+
+def oxford_comma(a)
+ if a.length > 1
+  
+   
+ else a.length
+   return a.join
+ 
+   
+ end
+end
+
+def each(arr)
+
 
 end
+
+
+puts oxford_comma(a1)
+puts oxford_comma(a2)
